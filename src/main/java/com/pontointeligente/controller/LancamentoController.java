5ceca84c87ad0ec16ea75b246fb8a70422f19e07
Allanlq <lanlq2009@gmail.com>
2020-07-29 22:55:02 -0300

    implementando metodo no lancamentocontroller

--- src/main/java/com/pontointeligente/controller/LancamentoController.java
+++ src/main/java/com/pontointeligente/controller/LancamentoController.java
@@ -1,21 +1,30 @@
 package com.pontointeligente.controller;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pontointeligente.controller.base.BaseController;
+import com.pontointeligente.controller.response.Response;
 import com.pontointeligente.controller.utils.ControllerHelper;
 import com.pontointeligente.domain.Lancamento;
 import com.pontointeligente.dto.LancamentoDto;
 import com.pontointeligente.services.FuncionarioService;
 import com.pontointeligente.services.LancamentoService;
+
+import net.sf.ehcache.search.Direction;
 
 
 @RestController
@@ -23,7 +32,6 @@ import com.pontointeligente.services.LancamentoService;
 @CrossOrigin(origins="*")
 public class LancamentoController extends BaseController {
 	
-	private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
 	@Autowired
 	private LancamentoService lancamentoService;
@@ -33,12 +41,26 @@ public class LancamentoController extends BaseController {
 	
 	@Value("${paginacao.qtd_por_pagina}")
 	private int totalDePaginas;
-	
-	
-	
-	
-	
-	
+		
+	@GetMapping(value="/funcionario/{funcionarioId}")
+	public ResponseEntity<Response<Page<LancamentoDto>>> listarPorFuncionarioId(@PathVariable("funcionarioId") Long funcionarioId,
+																				@RequestParam(value = "pag", defaultValue = "0") int pag,
+																				@RequestParam(value = "ord", defaultValue = "id") String ord,
+																				@RequestParam(value = "dir", defaultValue = "DESC") String dir){
+		
+		Response<Page<LancamentoDto>> response = new Response<Page<LancamentoDto>>();
+		
+		PageRequest pageRequest = PageRequest.of(pag, Integer.parseInt(ord),Sort.by(dir));
+		
+		Page<Lancamento> lancamentos = this.lancamentoService.findByFuncionarioId(funcionarioId, pageRequest);
+		
+		Page<LancamentoDto> lancamentoDtos = lancamentos.map(lancamento -> this.converterParaDto(lancamento));
+		
+		response.setData(lancamentoDtos);
+		
+		return ResponseEntity.ok(response);
+		
+	}
 	
 	
 	
