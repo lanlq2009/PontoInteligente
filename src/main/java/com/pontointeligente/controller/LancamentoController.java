ed3d13ef88c06a716422242b9ccb36e338884205
Allanlq <lanlq2009@gmail.com>
2020-07-30 23:23:07 -0300

    finalizando criação lancamentocontroller

--- src/main/java/com/pontointeligente/controller/LancamentoController.java
+++ src/main/java/com/pontointeligente/controller/LancamentoController.java
@@ -1,6 +1,9 @@
 package com.pontointeligente.controller;
 
 import java.text.ParseException;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +13,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +30,6 @@ import com.pontointeligente.domain.Lancamento;
 import com.pontointeligente.dto.LancamentoDto;
 import com.pontointeligente.services.FuncionarioService;
 import com.pontointeligente.services.LancamentoService;
-
-import net.sf.ehcache.search.Direction;
 
 
 @RestController
@@ -60,6 +65,98 @@ public class LancamentoController extends BaseController {
 		
 		return ResponseEntity.ok(response);
 		
+	}
+	
+	
+	
+	@GetMapping(value="/id/{id}")
+	public ResponseEntity<Response<LancamentoDto>> listarPorId(@PathVariable("id") Long id){
+		
+		Response<LancamentoDto> response = new Response<LancamentoDto>();
+		
+		Optional<Lancamento> lancamentos = this.lancamentoService.buscarPorId(id);
+		
+		if(!lancamentos.isPresent()) {
+			response.getErrors().add("Lancamento não encontrado "+ id);
+			ResponseEntity.badRequest().body(response);
+		}
+		
+		response.setData(this.converterParaDto(lancamentos.get()));
+		
+		return ResponseEntity.ok(response);
+	}
+	
+	
+	
+	@PostMapping
+	public ResponseEntity<Response<LancamentoDto>> adicionar(@Valid @RequestBody LancamentoDto lancamentoDto,  BindingResult result) throws ParseException{
+		
+		
+		Response<LancamentoDto> response = new Response<LancamentoDto>();
+		
+		this.settings();
+		
+		this.validator.validarLancamento(lancamentoDto, result);
+		if(result.hasErrors()) {
+		   result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+		   ResponseEntity.badRequest().body(response);
+		}
+		
+		Lancamento lancamento = this.converterParaLancamento(lancamentoDto, result);
+		
+		lancamento = this.lancamentoService.persistir(lancamento);
+		
+		response.setData(this.converterParaDto(lancamento));
+		
+		return ResponseEntity.ok(response);
+		
+	}
+	
+	
+	@PutMapping(value="/id/{id}")
+	public ResponseEntity<Response<LancamentoDto>> atualizar(@PathVariable("id") Long id,
+			                                                 @Valid @RequestBody LancamentoDto lancamentoDto, 
+			                                                 BindingResult result) throws ParseException {
+		
+		Response<LancamentoDto> response = new Response<LancamentoDto>();
+		
+		this.settings();
+		
+		this.validator.validarLancamento(lancamentoDto, result);
+		if(result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			ResponseEntity.badRequest().body(response);
+		}
+		
+        lancamentoDto.setId(Optional.of(id));
+        
+        Lancamento lancamento = this.converterParaLancamento(lancamentoDto, result);
+		
+		lancamento = this.lancamentoService.persistir(lancamento);
+		
+		response.setData(this.converterParaDto(lancamento));
+		
+		return ResponseEntity.ok(response);
+		
+	}
+	
+	
+	@DeleteMapping("/id/{id}")
+	public ResponseEntity<Response<String>> delete(@PathVariable("id") Long id) throws ParseException {
+
+		Response<String> response = new Response<String>();
+		
+		Optional<Lancamento> lancamento = this.lancamentoService.buscarPorId(id);
+		
+		if(!lancamento.isPresent()) {
+			response.getErrors().add("Não existe lancamento");
+			ResponseEntity.badRequest().body(response);
+		}
+		
+		this.lancamentoService.remover(lancamento.get());
+		
+		return ResponseEntity.ok(response);
+	
 	}
 	
 	
